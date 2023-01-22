3c79b612e2cecf2fa3b745d3613ef41a9a3145bc
David Wenzel <dwenzel95@gmail.com>
2023-01-21 19:48:16 -0600

    render a navlink for each section via map

--- life-divided-frontend/src/App.js
+++ life-divided-frontend/src/App.js
@@ -5,19 +5,13 @@ import Sections from "./components/Sections";
 
 
 
-
-
 function App() {
-// const [sections, setSections] = useState([]);
 
-// useEffect(() => {
-//   fetch("http://localhost:9292/sections")
-//     .then((res) => res.json())
-//     .then((data) => setSections(data));
-//     // console.log(sections)
-// }, []);
 
-// console.log(sections)
+
+
+
+  
   return (
     <div className="App">
       <Router>
--- life-divided-frontend/src/components/Sections.js
+++ life-divided-frontend/src/components/Sections.js
@@ -1,22 +1,32 @@
 import React from "react";
 // import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Sections() {
+
+    // state
   const [sections, setSections] = useState([]);
 
+
+
+    // fetch all sections with nested goals and setSections with that as data that came in from the fetch get request to /sections route
   useEffect(() => {
     fetch("http://localhost:9292/sections")
       .then((res) => res.json())
       .then((data) => setSections(data));
-    // console.log(sections)
   }, []);
 
-  console.log(sections);
+    //
+    const renderSections = sections.map((section) => (
+        <ul key={city.id}>
+            <NavLink to={`/cities/${section.id}`}>{city.name}</NavLink>
+        </ul>
+      ));
 
   return (
     <div>
-      <h1>hellow</h1>
+      <h1>Sections</h1>
     </div>
   );
 }
