4e6e9e4d8bd2121c0cfe64e14345b091945552da
David Wenzel <dwenzel95@gmail.com>
2023-01-21 20:29:43 -0600

    state moved back up to app component

--- life-divided-frontend/src/App.js
+++ life-divided-frontend/src/App.js
@@ -1,26 +1,31 @@
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Home from "./components/Home";
 import Sections from "./components/Sections";
 import Goals from "./components/Goals";
 
-
-
 function App() {
+  // state
+  const [sections, setSections] = useState([]);
 
-
-
-
-
+  // fetch all sections with nested goals and setSections with that as data that came in from the fetch get request to /sections route
+  useEffect(() => {
+    fetch("http://localhost:9292/sections")
+      .then((res) => res.json())
+      .then((data) => setSections(data));
+  }, []);
 
   return (
     <div className="App">
       <Router>
-      {/* <Navbar /> */}
-      <Switch>
-      <Route exact path="/" component={ Home }  />
-      <Route exact path="/sections" component={ Sections } />
-      <Route exact path="/sections/:id" component={ Goals } />
-      </Switch>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/sections">
+           <Sections sections={ sections } />
+          </Route>
+          <Route exact path="/sections/:id" component={Goals} />
+        </Switch>
       </Router>
     </div>
   );
--- life-divided-frontend/src/components/Sections.js
+++ life-divided-frontend/src/components/Sections.js
@@ -1,22 +1,9 @@
 import React from "react";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-export default function Sections() {
+export default function Sections({sections}) {
 
-    // state
-  const [sections, setSections] = useState([]);
-
-
-
-    // fetch all sections with nested goals and setSections with that as data that came in from the fetch get request to /sections route
-  useEffect(() => {
-    fetch("http://localhost:9292/sections")
-      .then((res) => res.json())
-      .then((data) => setSections(data));
-  }, []);
-
-    //map through each section and using dot notation create a navlink of each title to a component for that sections id
     const renderSections = sections.map((section) => (
         <li key={section.id}>
             <NavLink to={`/sections/${section.id}`}>{section.title}</NavLink>
