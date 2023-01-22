4ebcc2f952e05b78db9ee6746bacfe1c5e8bf2a8
David Wenzel <dwenzel95@gmail.com>
2023-01-21 18:45:30 -0600

    import home, router, and state and effect

--- life-divided-frontend/src/App.js
+++ life-divided-frontend/src/App.js
@@ -1,5 +1,15 @@
+import { useEffect, useState } from "react";
+import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+
+
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
     </div>
