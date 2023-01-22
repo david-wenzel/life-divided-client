595cd2ec254e8f004956b37a456fecd93794516c
David Wenzel <dwenzel95@gmail.com>
2023-01-21 22:14:26 -0600

    render edit goal function init

--- life-divided-frontend/src/components/GoalsCard.js
+++ life-divided-frontend/src/components/GoalsCard.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useState } from 'react';
+import GoalEditForm from './GoalEditForm';
 
 export default function GoalsCard({goal}) {
     const [isEdit, setIsEdit] = useState(false);
 
-    function handleEditClick(e, event) {
-        // console.log("updating");
+    function handleEditClick(e, goal) {
         setIsEdit(!isEdit);
-        // console.log(isEdit);
       }
 
 
@@ -22,7 +21,18 @@ export default function GoalsCard({goal}) {
 
 
 
-
+      const renderEditGoal = (
+        <div key={goal.id}>
+          {/* <button onClick={(e) => onDeleteClick(e, goal)}>X</button> &nbsp; */}
+          <button onClick={(e) => handleEditClick(e, goal)}>Edit</button> &nbsp;
+          <span style={{ fontWeight: "bold" }}>{goal.goal}</span>:{" "}
+          <GoalEditForm
+            event={goal}
+            onEditClick={handleEditClick}
+            // onEditGoal={onEditGoal}
+          />
+        </div>
+      );
 
 
 
