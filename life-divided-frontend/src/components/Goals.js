3a1f4ca9995d694ea6c0574e864bfd34cc2762ee
David Wenzel <dwenzel95@gmail.com>
2023-01-21 20:52:01 -0600

    rename found section to correspond

--- life-divided-frontend/src/components/Goals.js
+++ life-divided-frontend/src/components/Goals.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function Goals({sections}) {
+  
+  // useParams
   const { id } = useParams();
+  const params = useParams();
 
-
-
+  // find section with same id as id from the browser bar
+  // browswer bar returns a number that is a string so we to to parseInt to get it back to a integer to compare to our sections object
+  const foundSection = sections.find(({ id }) => id === parseInt(params.id));
 
 
 
